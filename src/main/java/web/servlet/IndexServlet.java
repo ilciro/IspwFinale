6892690095025e20aaaf4f0eaeb5a127ecc512e7
ilciro <73882655+ilciro@users.noreply.github.com>
2022-12-20 17:21:49 +0100

    initialize repo

---
+++ src/main/java/web/servlet/IndexServlet.java
@@ -0,0 +1,38 @@
+package web.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+    }
+
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String buttonA=request.getParameter("buttonA");
+		if(buttonA!=null && buttonA.equals("click"))
+		{
+			RequestDispatcher view=getServletContext().getRequestDispatcher("/pagina1.jsp");
+			view.forward(request, response);
+		}
+		}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang=en/it>
+<head>
+<meta charset="UTF-8">
+<title>Index page</title>
+</head>
+<body>
+
+<form action="IndexServlet" method="post">
+<div align="center">
+<input type="submit" id="buttonA" name="buttonA" value="click">
+</div>
+</form>
+
+</body>
+</html>
